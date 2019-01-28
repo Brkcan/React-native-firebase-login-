e594a1cc905c52556258b4e2e3ff5fb287b3ea14
Burak <36509701+Brkcan@users.noreply.github.com>
2019-01-28 11:12:50 +0300

    Update App.js

--- components/App.js
+++ components/App.js
@@ -1,11 +1,14 @@
 import React, { Components } from 'react';
-import {Text ,View} from 'react-native';
+import {Text ,View, Button} from 'react-native';
 import firebase from '@firebase/app';
 import '@firebase/auth';
 import Banner from './banner.js';
 import LoginForm from './loginForm.js';
 
 class App extends React.Component{
+state={
+	loggedIn : false
+}
 componentDidMount(){
 	firebase.initializeApp({
 		apiKey: 'AIzaSyA5mVFJG9am8_RlWJxYDtjoFDoJ3dRYMos',
@@ -15,16 +18,33 @@ componentDidMount(){
 		storageBucket: 'sahibinden-49786.appspot.com',
 		messagingSenderId: '233726739360'
 	});
+	firebase.auth().onAuthStateChanged((user) => {
+		const loggedIn = user ? true : false;
+		this.setState({
+			loggedIn
+		})
+	})
 }
+	renderContent(){
+		const {loggedIn} = this.state;
+		if(loggedIn){
+			return (
+				<Button onPress={() => firebase.auth().signOut()}
+				 title = 'Logout' color = '#E87B79'></Button>
+			)
+		}else{
+			return (
+				<LoginForm />
+			)
+		}
+	}
 	render(){
 		return (
 			<View>
 				<Banner text= 'Sahibinden' />
-				<LoginForm />
+				{this.renderContent()}
 			 </View>
-
 		);
 	}
 }
-
 export default App;
